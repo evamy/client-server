2d0490dfd840c08f4c6e4a7d8edd8aefa8d0f403
Zishan Ahmad <zishanahmad@outlook.com>
2014-11-08 16:10:15 +0530

    Added the client code

---
+++ src/networking/ZishanChatClient.java
@@ -0,0 +1,132 @@
+package networking;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+public class ZishanChatClient {
+	JTextField outgoing;
+	JTextArea incoming;
+	JTextField nameField;
+	JLabel nameLabel;
+	PrintWriter writer;
+	BufferedReader reader;
+	Socket sock;
+	String name;
+	
+	public static void main(String[] args) {
+		ZishanChatClient client = new ZishanChatClient();
+		client.go();
+	}
+	
+	public void go() {
+		JFrame frame = new JFrame("Zishan Chat Client");
+		JPanel header = new JPanel();
+		JPanel mainPanel = new JPanel();
+		JPanel footer = new JPanel();
+		outgoing = new JTextField(20);
+		incoming = new JTextArea(15,50);
+		incoming.setLineWrap(true);
+		incoming.setWrapStyleWord(true);
+		incoming.setEditable(false);
+		JScrollPane qScroller = new JScrollPane(incoming);
+		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		nameField = new JTextField(10);
+		nameField.setName("Bob");
+		nameLabel = new JLabel("Enter name: ");
+		JButton sendButton = new JButton("Send");
+		JButton setName = new JButton("Set");
+		sendButton.addActionListener(new SendButtonListener());
+		setName.addActionListener(new SetNameListener());
+		header.add(nameLabel);
+		header.add(nameField);
+		header.add(setName);
+		mainPanel.add(qScroller);
+		footer.add(outgoing);
+		footer.add(sendButton);
+		
+		setUpNetworking();
+		
+		Thread readerThread = new Thread(new IncomingReader());
+		readerThread.start();
+		
+		frame.getContentPane().add(BorderLayout.NORTH, header);
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		frame.getContentPane().add(BorderLayout.SOUTH, footer);
+	
+		frame.setSize(400, 500);
+		frame.setVisible(true);
+	}
+	
+	private void setUpNetworking() {
+		try {
+			sock = new Socket("127.0.0.1", 5000);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+			reader = new BufferedReader(streamReader);
+			writer = new PrintWriter(sock.getOutputStream());
+			System.out.println("Networking established");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public class SendButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			try {
+				String message = name + ": ";
+				message += outgoing.getText();
+				if(outgoing.getText().equals("bye")) {
+					message = outgoing.getText();
+					writer.println(message);
+					writer.flush();
+					System.exit(0);
+				}
+				writer.println(message);
+				writer.flush();
+				message = "";
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			outgoing.setText("");
+			outgoing.requestFocus();
+		}
+	}
+	
+	public class SetNameListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			name = nameField.getText();
+		}
+	}
+	
+	public class IncomingReader implements Runnable {
+		public void run() {
+			String message;
+			try {
+				while((message = reader.readLine()) != null) {
+					System.out.println(message);
+					incoming.append(message + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
