d3074f354dd06a97a906f0c37a5eafedd7602285
Zishan Ahmad <zishanahmad@outlook.com>
2014-11-14 09:53:36 +0530

    Renamed the files

--- src/networking/ZANTChatClient.java
+++ src/networking/ZANTChatClient.java
@@ -20,7 +20,7 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.text.DefaultCaret;
 
-public class ZishanChatClient {
+public class ZANTChatClient {
 	JTextField outgoing;
 	JTextArea incoming;
 	JTextField nameField;
@@ -31,36 +31,45 @@ public class ZishanChatClient {
 	String name;
 	
 	public static void main(String[] args) {
-		ZishanChatClient client = new ZishanChatClient();
-		client.go();
+		ZANTChatClient client = new ZANTChatClient();
+		client.start();
 	}
 	
-	public void go() {
-		JFrame frame = new JFrame("Zishan Chat Client");
+	public void start() {
+		JFrame frame = new JFrame("ZANT Chat Client");
 		JPanel header = new JPanel();
 		JPanel mainPanel = new JPanel();
 		JPanel footer = new JPanel();
+		
 		outgoing = new JTextField(20);
 		incoming = new JTextArea(15,50);
 		incoming.setLineWrap(true);
 		incoming.setWrapStyleWord(true);
 		incoming.setEditable(false);
+		
 		DefaultCaret caret = (DefaultCaret)incoming.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		
 		JScrollPane qScroller = new JScrollPane(incoming);
 		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		
 		nameField = new JTextField(10);
 		nameField.setName("Bob");
 		nameLabel = new JLabel("Enter name: ");
+		
 		JButton sendButton = new JButton("Send");
 		JButton setName = new JButton("Set");
+		
 		sendButton.addActionListener(new SendButtonListener());
 		setName.addActionListener(new SetNameListener());
+		
 		header.add(nameLabel);
 		header.add(nameField);
 		header.add(setName);
+		
 		mainPanel.add(qScroller);
+		
 		footer.add(outgoing);
 		footer.add(sendButton);
 		
@@ -94,13 +103,20 @@ public class ZishanChatClient {
 	public class SendButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 			try {
-				String message = name + ": ";
-				message += outgoing.getText();
+				String message;
 				if(outgoing.getText().equals("bye")) {
 					message = outgoing.getText();
 					writer.println(message);
 					writer.flush();
 					System.exit(0);
+				}
+				int pos = outgoing.getText().indexOf("[");
+				if(pos == 0) {
+					String rec = outgoing.getText().substring(0, outgoing.getText().indexOf("]") + 1);
+					String mes = outgoing.getText().substring(outgoing.getText().indexOf("]") + 1);
+					message = rec + name + ":" + mes;
+				} else {
+					message = name + ": " + outgoing.getText();
 				}
 				writer.println(message);
 				writer.flush();
@@ -116,6 +132,9 @@ public class ZishanChatClient {
 	public class SetNameListener implements ActionListener {
 		public void actionPerformed(ActionEvent ev) {
 			name = nameField.getText();
+			String nameSend = "$^@" + name;
+			writer.println(nameSend);
+			writer.flush();
 		}
 	}
 	
@@ -124,6 +143,8 @@ public class ZishanChatClient {
 			String message;
 			try {
 				while((message = reader.readLine()) != null) {
+					int pos = message.indexOf("$^@");
+					if(pos != -1) continue;
 					System.out.println(message);
 					incoming.append(message + "\n");
 				}
--- src/networking/ZANTChatServer.java
+++ src/networking/ZANTChatServer.java
@@ -7,17 +7,24 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ListIterator;
 
-public class ZishanChatServer {
+import javax.sound.midi.Receiver;
+
+public class ZANTChatServer {
 	ArrayList<PrintWriter> clientOutputStreams;
+	HashMap<String, PrintWriter> map;
+	int noc;
 	
 	public static void main(String[] args) {
-		new ZishanChatServer().go();
+		new ZANTChatServer().start();
 	}
 	
-	public void go() {
+	public void start() {
 		clientOutputStreams = new ArrayList<PrintWriter>();
+		map = new HashMap<String, PrintWriter>();
+		noc = -1;
 		try {
 			ServerSocket serverSock = new ServerSocket(5000);
 			while(true) {
@@ -27,7 +34,9 @@ public class ZishanChatServer {
 				
 				Thread t = new Thread(new ClientHandler(clientSocket));
 				t.start();
+				noc++;
 				System.out.println("Got a connection at " + clientSocket.toString());
+				System.out.println("Clients connected: " + (noc+1));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -52,8 +61,33 @@ public class ZishanChatServer {
 			String message;
 			try {
 				while((message = reader.readLine()) != null) {
+					int pos = message.indexOf("$^@");
+					if(pos == 0) {
+						String name = message.substring(3);
+						System.out.println(name);
+						map.put(name, clientOutputStreams.get(noc));
+						//System.out.println(name + " : " + map.get(name));
+						continue;
+					}
+					pos = message.indexOf("[");
+					//System.out.println("Pos: " + pos);
+					if(pos == 0) {
+						String recipients = message.substring(1, message.indexOf("]"));
+						//System.out.println(recipients);
+						message = message.substring(message.indexOf("]") + 1);
+						String name = message.substring(0, message.indexOf(":"));
+						recipients += "," + name;
+						for (String rec : recipients.split(",")) {
+							//System.out.println(rec);
+							PrintWriter writer = map.get(rec);
+							writer.println(message);
+							writer.flush();
+						}
+						System.out.println("reading: " + message);
+						continue;
+					}
 					System.out.println("read: " + message);
-					tellEveryone(message);
+					broadcast(message);
 					if(message.equals("bye")) {
 						sock.close();
 						break;
@@ -66,7 +100,7 @@ public class ZishanChatServer {
 		}
 	}
 	
-	public void tellEveryone(String message) {
+	public void broadcast(String message) {
 		ListIterator<PrintWriter> it = clientOutputStreams.listIterator();
 		while(it.hasNext()) {
 			try {
